9cc98fae1a2aaefd414e4cad761ef31028edbbf2
UmedJadhav <umedj.formal@gmail.com>
2020-10-13 17:18:15 +0530

    work: Augmentaed the form validation

--- src/components/Auth/Register.component.jsx
+++ src/components/Auth/Register.component.jsx
@@ -56,19 +56,28 @@ class Register extends Component {
     }
 
     handleSubmit = event => {
+        event.preventDefault();
         if(this.isFormValid()){
+            this.setState({ errors:[], loading: true});
 
-            event.preventDefault();
             firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
             .then( createdUser => {
                 console.log(createdUser);
+                this.setState({ loading:false });
             })
-            .catch(err => console.error(err));
+            .catch(err => {
+                console.error(err);
+                this.setState({ errors: this.state.errors.concat(err), loading:false  });
+            });
         }
     }
 
+displayErrors = errors => errors.map((error, i) => <p key={i}>{ error.message}</p>);
+
+    handleInputErrors  = (errors, inputName) => (errors.some(error => error.message.toLowerCase().includes(inputName)) ? 'error' : '');
+
     render() {
-        const { username, email, password, passwordConfirmation } = this.state;
+        const { username, email, password, passwordConfirmation, errors, loading } = this.state;
         
         return (
             <Grid textAlign='center' verticalAlign='middle' className='app'>
@@ -76,13 +85,18 @@ class Register extends Component {
                     <Header as='h2' icon color='orange' textAlign='center'>
                         <Icon name='puzzle piece' color='orange'/>
                     </Header>
+                        {
+                            errors.length > 0 && (<Message error>
+                                <h4>{this.displayErrors(errors) }</h4> 
+                            </Message>)
+                        }
                     <Form size='large' onSubmit={ this.handleSubmit }>
                         <Segment stacked>
-                            <Form.Input fluid name='username' icon='user' iconPosition='left' placeholder='Username' onChange={ this.handleChange } type='text' value={username} />
-                            <Form.Input fluid name='email' icon='mail' iconPosition='left' placeholder='Email' onChange={ this.handleChange } type='email' value={email} />
-                            <Form.Input fluid name='password' icon='user' iconPosition='left' placeholder='Password' onChange={ this.handleChange } type='password' value={password} />
-                            <Form.Input fluid name='passwordConfirmation' icon='repeat' iconPosition='left' placeholder='Confirm Password' onChange={ this.handleChange } type='password' value={ passwordConfirmation }/>
-                            <Button color='orange' fluid size='large'>Submit</Button>
+                            <Form.Input fluid className = { this.handleInputErrors(errors, 'username')} name='username' icon='user' iconPosition='left' placeholder='Username' onChange={ this.handleChange } type='text' value={username} />
+                            <Form.Input fluid className = { this.handleInputErrors(errors, 'email')} name='email' icon='mail' iconPosition='left' placeholder='Email' onChange={ this.handleChange } type='email' value={email} />
+                            <Form.Input fluid className = { this.handleInputErrors(errors, 'password') } name='password' icon='user' iconPosition='left' placeholder='Password' onChange={ this.handleChange } type='password' value={password} />
+                            <Form.Input fluid className = { this.handleInputErrors(errors, 'password') } name='passwordConfirmation' icon='repeat' iconPosition='left' placeholder='Confirm Password' onChange={ this.handleChange } type='password' value={ passwordConfirmation }/>
+                            <Button color='orange' fluid size='large' className= { loading? 'loading' : ''} disabled = {loading} >Submit</Button>
                         </Segment>
                     </Form>
                     <Message>Already a user? <Link to='/login'>Login</Link></Message>
