26b48302dc17c4b0c0846c84b7024866f39f4e0e
UmedJadhav <umedj.formal@gmail.com>
2020-10-13 18:41:47 +0530

    work: Setup Redux and made userData available globally

--- src/index.js
+++ src/index.js
@@ -5,15 +5,22 @@ import App from './components/App/App';
 import { BrowserRouter as Router , Switch , Route, withRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import firebase from './firebase';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
 
 import Login from './components/Auth/Login.component';
 import Register from './components/Auth/Register.component';
+import rootReducer from './reducers/reducers';
+import { setUser } from './actions/actions';
 
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 class Root extends Component{
   componentDidMount() {
     firebase.auth().onAuthStateChanged( user => {
       if(user){
+        this.props.setUser(user);
         this.props.history.push('/');
       }
     });
@@ -31,12 +38,14 @@ class Root extends Component{
   }
 } 
 
-const RootwithAuth = withRouter(Root);
+const RootwithAuth = withRouter(connect(null,{ setUser })(Root));
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <RootwithAuth />
-    </Router>
+    <Provider store={store} >
+      <Router>
+        <RootwithAuth />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
