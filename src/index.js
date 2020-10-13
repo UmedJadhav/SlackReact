b6689bd7fd4056262bdddef3750cf7e8e988ea06
UmedJadhav <umedj.formal@gmail.com>
2020-10-13 18:07:40 +0530

    work:Implemented onAuthStateChanged listener

--- src/index.js
+++ src/index.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import { BrowserRouter as Router , Switch , Route } from 'react-router-dom';
+import { BrowserRouter as Router , Switch , Route, withRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
+import firebase from './firebase';
 
 import Login from './components/Auth/Login.component';
 import Register from './components/Auth/Register.component';
 
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={ App }/>
-      <Route path="/register" component={ Register }/>
-      <Route path="/login" component={ Login }/>
-    </Switch>
-  </Router>
-);
+class Root extends Component{
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged( user => {
+      if(user){
+        this.props.history.push('/');
+      }
+    });
+  }
+  
+  render(){
+      return (
+                <Switch>
+                  <Route exact path="/" component={ App }/>
+                  <Route path="/register" component={ Register }/>
+                  <Route path="/login" component={ Login }/>
+                </Switch>
+              )
 
+  }
+} 
 
+const RootwithAuth = withRouter(Root);
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <Router>
+      <RootwithAuth />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
