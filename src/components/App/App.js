b82a2a443a86a06f5de794675b47a15ad85a9b13
UmedJadhav <umedj.formal@gmail.com>
2020-10-14 08:59:52 +0530

    work: Added basic scafold of the App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      aPPas
-    </div>
-  );
-}
+import ColorPanel from '../ColorPanel/ColorPanel.component';
+import Messages from '../Messages/Messages.component';
+import SidePanel from '../SidePanel/SidePanel.component';
+import MetaPanel from '../MetaPanel/MetaPanel.component';
+
+const App = () => (
+  <Grid columns='equal' className = 'app' style={ {background: '#eee' } }>
+    <ColorPanel />
+    <SidePanel />
+    
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages />
+    </Grid.Column>
+
+    <Grid.Column width={4} >
+      <MetaPanel />
+    </Grid.Column>
+  </Grid>
+);
 
 export default App;
