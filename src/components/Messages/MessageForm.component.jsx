57dc2c54ba85922b3da03ec6c157e5ce9f5d56a0
UmedJadhav <umed.jadhav@somaiya.edu>
2020-10-15 11:36:28 +0530

    work: Created a FileModal Component for uploading Images

--- src/components/Messages/MessageForm.component.jsx
+++ src/components/Messages/MessageForm.component.jsx
@@ -2,14 +2,21 @@ import React, { Component } from 'react'
 import { Segment, Button, Input} from 'semantic-ui-react';
 import firebase from '../../firebase';
 
+import FileModal from './FileModal.component';
+
 export default class MessageForm extends Component {
     state = {
         message: '',
         loading: false,
         channel: this.props.currentChannel,
         user: this.props.currentUser,
-        errors: []
+        errors: [],
+        modal: false
     }
+
+    openModal = () => this.setState({ modal: true });
+    
+    closeModal = () => this.setState({ modal: false });
 
     handleChange = event => {
         this.setState({
@@ -56,7 +63,7 @@ export default class MessageForm extends Component {
     }
 
     render() {
-        const { errors, message, loading } = this.state;
+        const { errors, message, loading, modal } = this.state;
         return (
             <Segment class='message__form'>
                 <Input fluid name='message' style= {{ marginBottom:'0.7em' }}  value={ message }
@@ -66,7 +73,9 @@ export default class MessageForm extends Component {
                 <Button.Group icon widths='2'>
                     <Button color='orange' content='Add Reply' labelPosition='left' icon='edit' disabled={ loading }
                     onClick={ this.sendMessage }/>
-                    <Button color='teal' content='Upload Media' labelPosition='right' icon='cloud upload'/>
+                    <Button color='teal' content='Upload Media' labelPosition='right' icon='cloud upload' 
+                    onClick={ this.openModal }/>
+                    <FileModal modal ={modal} closeModal={ this.closeModal }/>
                 </Button.Group>
             
             </Segment>
