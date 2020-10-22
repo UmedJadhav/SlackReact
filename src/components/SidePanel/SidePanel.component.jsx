af33e081b75d0b86e4a925784c08aaccad72986d
UmedJadhav <umed.jadhav@somaiya.edu>
2020-10-22 08:54:38 +0530

    work: Created the starred component to hold all the starred channels

--- src/components/SidePanel/SidePanel.component.jsx
+++ src/components/SidePanel/SidePanel.component.jsx
@@ -4,6 +4,7 @@ import { Menu } from 'semantic-ui-react';
 import UserPanel  from './UserPanel.component';
 import Channels  from './Channels.component';
 import DirectMessages  from './DirectMessages.component';
+import Starred from './Starred.component';
 
 export default class SidePanel extends Component {
     render() {
@@ -11,6 +12,7 @@ export default class SidePanel extends Component {
         return (
         <Menu size='large' inverted  fixed='left' vertical style={{ background: '#4c3c43', fontSize: '1.2rem'}}>
             <UserPanel currentUser = {currentUser} />
+            <Starred />
             <Channels currentUser = {currentUser} />
             <DirectMessages currentUser = {currentUser} />
         </Menu>
