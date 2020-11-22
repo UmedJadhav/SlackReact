ee25b78474a4fcf1fb7102c70d5b44662cde901b
UmedJadhav <umed.jadhav@somaiya.edu>
2020-11-22 23:14:34 +0530

    work: Created basic Modal layout for changing avatar

--- src/components/SidePanel/UserPanel.component.jsx
+++ src/components/SidePanel/UserPanel.component.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
+import { Grid, Header, Icon, Dropdown, Image, Modal, Input, Button } from 'semantic-ui-react';
 import firebase from '../../firebase'
 
 class UserPanel extends Component {
 
     state = {
-        user: this.props.currentUser
+        user: this.props.currentUser,
+        modal: false
     }
+
+    openModal = () => this.setState({ modal: true })
+    
+    closeModal = () => this.setState({ modal: false })
 
     dropdownOptions = () => [
         {
@@ -16,7 +21,7 @@ class UserPanel extends Component {
         },
         {
             key:'avatar',
-            text: <span>Change Avatar</span>
+            text: <span onClick={ this.openModal }>Change Avatar</span>
         },
         {   
             key: 'signout',
@@ -31,7 +36,7 @@ class UserPanel extends Component {
 
     render() {
 
-        const { user } = this.state
+        const { user, modal } = this.state
         return (
             <Grid style={{ background: this.props.primaryColor}}>
                 <Grid.Column>
@@ -42,16 +47,46 @@ class UserPanel extends Component {
                         </Header>
                     </Grid.Row>
 
-                    <Header style={{ padding: '1.25em' }} as='h4' inverted>
-                        <Dropdown trigger={
-                            <span>
-                                <Image src={ user.photoURL } spaced='right' avatar/>
-                                { user.displayName }
-                            </span>
-                        }
-                        options = { this.dropdownOptions() }/>
-                    </Header>
+                    {/* <Grid.Row> */}
+                        <Header style={{ padding: '1.25em' }} as='h4' inverted>
+                            <Dropdown trigger={
+                                <span>
+                                    <Image src={ user.photoURL } spaced='right' avatar/>
+                                    { user.displayName }
+                                </span>
+                            }
+                            options = { this.dropdownOptions() }/>
+                        </Header>
+                    {/* </Grid.Row> */}
+                    <Modal basic open={modal} onClose={this.closeModal}>
+                        <Modal.Header>Change Avatar</Modal.Header>
+                        <Modal.Content>
+                            <Input fluid type='file' label='New Avatar' name='previewImage'/>
+                            <Grid centered stackable columns={2}>
+                                <Grid.Row centered>
+                                    <Grid.Column className='ui center aligned grid'>
+                                        
+                                    </Grid.Column>
+                                    <Grid.Column>
+
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Modal.Content>
+                        <Modal.Actions>
+                            <Button color='green' inverted>
+                                <Icon name='save'/> Change Avatar
+                            </Button>
+                            <Button color='green' inverted>
+                                <Icon name='image'/> Preview
+                            </Button>
+                            <Button color='red' inverted onClick={ this.closeModal }>
+                                <Icon name='remove'/> Cancel
+                            </Button>
+                        </Modal.Actions>
+                    </Modal>
                 </Grid.Column>
+
             </Grid>
         );
     }
